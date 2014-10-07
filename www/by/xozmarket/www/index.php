2f0dbbc0c3810ac4bde05bd97b9e943d7180465c
dzmitry.karniyenka <twegostar@gmail.com>
2014-10-07 13:39:38 +0300

    Add index.php.

---
+++ www/by/xozmarket/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title><?php echo $GLOBALS["host"]->name . "." . $GLOBALS["host"]->host . "." .$GLOBALS["host"]->zone; ?></title>
+    </head>
+    <body>
+    Hello world!
+    </body>
+</html>
+
+
+
