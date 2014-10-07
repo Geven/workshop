e3d90e6e6aec475606c393210aac666818e55b10
dzmitry.karniyenka <twegostar@gmail.com>
2014-10-07 12:23:44 +0300

    Add checking of presence of a map.json.

--- common/Router.php
+++ common/Router.php
@@ -8,5 +8,14 @@ class Router {
         $this->root = "/www/$host->zone/$host->host/$host->name";
         $this->path = $request->path;
         $this->query = $request->query;
+
+        $data = file_get_contents($_SERVER["DOCUMENT_ROOT"] . $this->root . "/map.json");
+
+        if($data) {
+
+        }
+        else {
+            exit("ERROR: can't find map.json.");
+        }
     }
 }
